11ed7af7994aea4988023777a20ecd1aeb623413
VIN <v3p51435@gmail.com>
2024-09-04 12:27:03 +0530

    unsplash error alert and css update

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,12 @@ function App() {
   const handleSearch = async () => {
     if (query) {
       try {
-        const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${import.meta.env.VITE_UNSPLASH_API_KEY}&per_page=30`);
+        const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${import.meta.env.VITE_UNSPLASH_API_KEY}&per_page=20`);
         const data = await response.json();
         setPhotos(data.results);
       } catch (error) {
         console.error('Error fetching the photos:', error);
+        alert(error)
       }
     }
   };
